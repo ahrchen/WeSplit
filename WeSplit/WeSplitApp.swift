1725a6dd091dbdcd4d024ddaab275c11a48b6af7
Raymond Chen <ahrchen@gmail.com>
2022-01-31 14:51:26 -0800

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Raymond Chen on 1/31/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
